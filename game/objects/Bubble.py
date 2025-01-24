ac2fbe16c73a45f2fa313082b65ba9190be0d2c2
Mateusz Chojnacki <matchojnacki@protonmail.com>
2025-01-24 22:11:22 +0100

    make more bubbles (without optimizing)

--- game/objects/Bubble.py
+++ game/objects/Bubble.py
@@ -1,3 +1,4 @@
+import math
 import pygame
 
 import common.Object
@@ -5,17 +6,21 @@ import common.Object
 
 class Bubble(common.Object):
 
-    def __init__(self):
-        super().__init__("BubbleObject", "assets/sprites/bubble.png")
-        self.initial_position = (640, 360)
-        self.position = self.initial_position
+    def __init__(self, velocity, angle, image):
+        super().__init__("BubbleObject", image)
+        self.center = (640, 360)
+        self.velocity = velocity
+        self.angle = math.radians(angle)
+
         self.lifetime = 0
         self.update()
 
     def update(self):
         self.lifetime += 1
-        self.position = (640 - self.lifetime / 2, 360 - self.lifetime / 2)
+        self.center = (self.center[0] + self.velocity * math.cos(self.angle),
+                       self.center[1] + self.velocity * math.sin(self.angle))
 
     def render(self, window, position_translation = (0, 0)):
         image = pygame.transform.scale(self.img.image, (self.lifetime, self.lifetime))
-        window.blit(image, self.position + position_translation)
+        position = (self.center[0] - self.lifetime / 2, self.center[1] - self.lifetime / 2)
+        window.blit(image, position + position_translation)
--- game/scenes/Game.py
+++ game/scenes/Game.py
@@ -1,7 +1,10 @@
+import random
+
 import pygame
 
 import audio.Sound
 import common.Scene
+import drawable.Image
 import game.objects.Background
 import game.objects.Bubble
 
@@ -12,14 +15,21 @@ class Game(common.Scene):
         super().__init__("GameMainScene", window)
 
         self.background = game.objects.Background()
-        self.bubble = game.objects.Bubble()
+        self.bubble_image = drawable.Image("bubble_image", "assets/sprites/bubble.png")
+        self.bubbles = []
 
     def process_input(self, keyboard_input, joystick, mouse_input, mouse_position):
         pass
 
     def update(self):
-        self.bubble.update()
+        for bubble in self.bubbles:
+           bubble.update()
+        if len(self.bubbles) < 300:
+            new_bubble = game.objects.Bubble(random.randint(1, 5), random.randint(0, 359),
+                                             self.bubble_image)
+            self.bubbles.append(new_bubble)
 
     def render(self, color=pygame.Color(255, 255, 255, 255)):
         self.background.render(self.window.window)
-        self.bubble.render(self.window.window)
+        for bubble in self.bubbles:
+            bubble.render(self.window.window)
